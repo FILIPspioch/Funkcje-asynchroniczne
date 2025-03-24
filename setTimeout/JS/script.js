f51b879b61e5f748612e60ca019edd1ba5b28b20
FilipSpioch <biedaczeq@gmail.com>
2025-03-24 22:19:42 +0100

    commit 3

--- setTimeout/JS/script.js
+++ setTimeout/JS/script.js
@@ -1,8 +1,19 @@
 //elementy
 const viewport = document.querySelector("#main");
 
+//zmienne
+const date = new Date();
+
 //funkcje
-function sayHello() {}
+function sayHello() {
+  viewport.innerHTML += `<h1>tekst</h1>`;
+}
 
 //kod
-setTimeout();
+viewport.innerHTML += ` Godzina drukowana przed wywyołaniem funkcji ${date.getHours()} : ${date.getMinutes()}`;
+setTimeout(sayHello, 1000);
+viewport.innerHTML += `<br></br>`;
+viewport.innerHTML += ` Godzina drukowana po wywyołaniu funkcji ${date.getHours()} : ${date.getMinutes()}`;
+
+viewport.innerHTML += `<h2> Wniosek: </h2>`;
+viewport.innerHTML += `<p>Program nie czeka na to aż się wykona funkcja setTimeout tylko idzie dalej i gdy minie podany czas to się wykonuje.</p>`;
